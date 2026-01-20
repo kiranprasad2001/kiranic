240bbf252fe845e2272e4191b8ec4047b4dc25ca
Kiran Prasad <kiranprasad2001@gmail.com>
2026-01-19 21:55:41 -0500

    feat(seo): implementation of publisher strategy (sitemap, pillars, glossary)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kiranic.pages.dev',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   output: 'static',
   adapter: cloudflare()
 });
